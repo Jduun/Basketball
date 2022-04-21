f4d92786c7c04d0cbe19d0545c3f3694e1a3312f
HelloWorld <isaevstanislav2003@mail.ru>
2022-04-21 13:43:12 +0300

    а

--- Basketball/Basketball.cpp
+++ Basketball/Basketball.cpp
@@ -4,33 +4,6 @@
 
 int main()
 {
-	/*
-	const int HEIGHT = 600;
-	const int WIDTH = 1120;
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), "Basketball");
-	window->setFramerateLimit(60);
-	Ball ball(WIDTH / 2 - 5, HEIGHT / 2 - 5, sf::Color(180, 50, 0), 5);
-	while (window->isOpen())
-	{
-		sf::Event e;
-		while (window->pollEvent(e))
-		{
-			if (e.type == sf::Event::Closed)
-			{
-				window->close();
-			}
-		}
-		window->clear(sf::Color(255, 160, 122));
-		if (ball.Pass(200, 400))
-		{
-
-		}
-		ball.Update(window);
-		ball.Draw(window);
-		window->display();
-	}
-	delete window;
-	*/
 	Game* game = new Game();
 	game->Play();
 	delete game;
